b78f4509faf61403a0a2d19b533d3702a0dadfa7
mehboob-tx <mehboob@techcreatix.com>
2020-02-25 11:56:09 +0500

    chk login data from db

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -48,19 +48,59 @@ class LoginController extends Controller
     {
         $this->validateLogin($request);
 
-        User::create($request->all());
-        return redirect('admin')
-                    ->with('success',' Successfully');
+
+        $email = $request->input('email');
+         $password = $request->input('password');
+
+         $user = User::where('email', '=', $email)->first();
+         if (!$user) {
+            return response()->json(['success'=>false, 'message' => 'Login Fail, please check email id']);
+         }
+         if (\Hash::check($password, $user->password)) {
+            return response()->json(['success'=>false, 'message' => 'Login Fail, pls check password']);
+         }
+            return response()->json(['success'=>true,'message'=>'success', 'data' => $user]);
+
+
+
+
+    /*  $chk  = $request->only('email', 'password');
+
+    if (Auth::attempt($chk)) {
+        // Authentication passed...
+        return redirect('admin');
+    }*/
+
+
+
+
+       /*  $email = $request->input('email');
+         $password = $request->input('password');
+
+         $user = User::where('email', '=', $email)->first();
+         if (!$user) {
+            return response()->with([ 'message' => 'Login Fail, please check email id']);
+         }
+         if (!Hash::check($password, $user->password)) {
+            return response()->with([ 'message' => 'Login Fail, pls check password']);
+         }
+            return response()->with(['success'=>true,'message'=>'success', 'data' => $user])*/  
+
+       /*User::create($request->all());
+        $request->session()->put('email',$request->input('email'));
+        $myemail=$request->session()->get('email');
+        return redirect('login')
+                    ->with('email',$myemail);
 
        
 
         if ($this->attemptLogin($request)) {
             return $this->sendLoginResponse($request);
-        }
+        }*/
 
         
 
-        return $this->sendFailedLoginResponse($request);
+      //  return $this->sendFailedLoginResponse($request);
     }
     public function __construct()
     {
--- resources/views/admin-frontend/admin-index.blade.php
+++ resources/views/admin-frontend/admin-index.blade.php
@@ -4,7 +4,6 @@
 @stop
 @section('content')
 
-
   <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
     <!-- Content Header (Page header) -->
